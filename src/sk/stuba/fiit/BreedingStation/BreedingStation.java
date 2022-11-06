ee34fa5dc4e031638e91d09da8272b3b1fa60120
unknown <jakub.martinak@outlook.com>
2022-11-06 23:10:25 +0100

    PetInfo and Tasks

--- src/sk/stuba/fiit/BreedingStation/BreedingStation.java
+++ src/sk/stuba/fiit/BreedingStation/BreedingStation.java
@@ -9,13 +9,14 @@ public class BreedingStation {
 	private int day = 0;
 	private int num_of_events;
 	private int num_of_pets;
-	List<Pet> pets = new ArrayList<Pet>();
+	List<Dog> dogs = new ArrayList<Dog>();
+	List<Cat> cats = new ArrayList<Cat>();
 
 	public BreedingStation(String name, int num_of_pets, List<String> names) {
 		this.name = name;
 		this.num_of_pets = num_of_pets;
 		for (int i = 0; i < this.num_of_pets; i++) {
-			pets.add(new Dog(names.get(i), 1));
+			dogs.add(new Dog(names.get(i), 1));
 			// System.out.println(names.get(i));
 		}
 	}
@@ -32,7 +33,8 @@ public class BreedingStation {
 		return this.day;
 	}
 
-	public void SetPet() {
+	public void SetPet(int n) {
+		this.num_of_pets = n;
 		// TODO Code for adding or subtracting pets to/from Station
 	}
 
@@ -42,5 +44,26 @@ public class BreedingStation {
 
 	public void GetName() {
 		System.out.println(this.name);
+	}
+
+	public void GetPetInfo(int n, char p) {
+		if (p == 'd') {
+			System.out.printf("Dog %S INFO: %n", dogs.get(n).GetName());
+			System.out.printf("Has %s happiness %n", dogs.get(n).GetHappiness());
+			System.out.printf("Has %s health %n", dogs.get(n).GetHealth());
+			System.out.printf("Has %s hunger %n", dogs.get(n).GetHunger());
+			if (dogs.get(n).GetCleanliness()) {
+				System.out.printf("%s is clean %n", dogs.get(n).GetName());
+			} else {
+				System.out.printf("%s needs to be showered %n", dogs.get(n).GetName());
+			}
+			if (dogs.get(n).GetHydration()) {
+				System.out.printf("%s is hydrated. %n", dogs.get(n).GetName());
+			} else {
+				System.out.printf("%s need his bowl of water refilled. %n", dogs.get(n).GetName());
+			}
+			System.out.printf("Has %s happiness %n", dogs.get(n).GetHappiness());
+
+		}
 	}
 }
--- src/sk/stuba/fiit/hlavny_loop/GameLoop.java
+++ src/sk/stuba/fiit/hlavny_loop/GameLoop.java
@@ -12,6 +12,9 @@ public class GameLoop {
 
 	public static void main(String[] args) {
 
+		String[] tasks = { "Go for a walk", "Shower", "Refill bowl of water", "Give Food", "Give Snack", "Play Fetch",
+				"Pet", "Cuddle", "Buy a toy", "cut the claws", "cut the fur" };
+
 		System.out.println("==================================================================");
 		System.out.println("Hello there! Welcome to the Game MyPets!");
 		System.out.println("In this game you will take care of you Pets in Breeding station.");
@@ -30,9 +33,29 @@ public class GameLoop {
 		BreedingStation station = new BreedingStation(name, num_of_petsInStation, names);
 
 		System.out.printf("Your station %s be Starting with %s Pets. %n", name, num_of_petsInStation);
+		System.out.println("Every day you can do 10 tasks after 10 tasks your day ends. So choose carefully.");
 		System.out.println("Let's the journey begin!");
 		System.out.println("==================================================================");
 
+		int num_of_tasks = 0;
+
+		// station.GetPetInfo(0, 'd');
+
+		while (station.GetPet() > 0) {
+			System.out.println("========================");
+			System.out.printf("%-5S Day %s", "", station.GetDay());
+			System.out.println("========================");
+
+			while (num_of_tasks < 4) {
+				for (int i = 0; i < tasks.length; i++) {
+					System.out.printf("%s - %s %n", i, tasks[i]);
+				}
+				int task = input.nextInt();
+
+			}
+
+			station.SetPet(0);
+		}
 	}
 
 }
